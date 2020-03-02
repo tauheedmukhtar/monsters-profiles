c354c6acfddb53d4814954329fb70414b0af7bc9
Tauheed Mukhtar <tauheed.mukhtar@gmail.com>
2020-03-03 02:16:18 +0500

    Updates

---
+++ precache-manifest.afe8cc304647bbdfb555d9d0ea8e0911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91360dd54910168ccb22ebbfc5b6c5e2",
+    "url": "/monsters-profiles/index.html"
+  },
+  {
+    "revision": "35ce1684d54e17d74ab3",
+    "url": "/monsters-profiles/static/css/main.f53f6881.chunk.css"
+  },
+  {
+    "revision": "ba763987080263a5c2fc",
+    "url": "/monsters-profiles/static/js/2.0f55060e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-profiles/static/js/2.0f55060e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35ce1684d54e17d74ab3",
+    "url": "/monsters-profiles/static/js/main.1361bf50.chunk.js"
+  },
+  {
+    "revision": "24dec6251303764b0eeb",
+    "url": "/monsters-profiles/static/js/runtime-main.c18f7acc.js"
+  }
+]);
